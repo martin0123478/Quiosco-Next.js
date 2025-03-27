e116cb7e8a0b80174e184307413568b9fd7468cf
martin0123478 <martin_agui97@outlook.es>
2025-03-27 14:02:07 -0600

    feat:save product in database

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -3,21 +3,34 @@ import React from 'react'
 import ProductForm from './ProductForm'
 import { ProductSchema } from '@/src/schema'
 import { toast } from 'react-toastify'
+import { createProduct } from '@/actions/create-product-action'
+import { useRouter } from 'next/navigation'
 
 export const AddProductForm = ({ children }: { children: React.ReactNode }) => {
-
+    const router = useRouter()
     const handleSubmit = async (formData: FormData) => {
         const data = {
             name: formData.get('name'),
             price: formData.get('price'),
-            categoryId: formData.get('categoryId')
+            categoryId: formData.get('categoryId'),
+            image: formData.get('image')
         }
         const result = ProductSchema.safeParse(data)
         if (!result.success) {
             result.error.issues.forEach(issue => {
                 toast.error(issue.message)
             })
+            return
         }
+        const response = await createProduct(result.data)
+        if (response?.errors) {
+            response.errors.forEach(issue => {
+                toast.error(issue.message)
+            })
+            return
+        }
+        toast.success('Producto creado correctamente')
+        router.push('/admin/products')
 
     }
     return (
